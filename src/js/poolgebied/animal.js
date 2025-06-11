9d40f58722e420093942861c929a05770d41f7a0
maudd <1091860@hr.nl>
2025-06-11 15:06:19 +0200

    connection with resources

--- src/js/poolgebied/animal.js
+++ src/js/poolgebied/animal.js
@@ -1,6 +1,6 @@
 import '../css/style.css'
 import { Actor, Engine, Vector, DisplayMode } from "excalibur"
-import { Resources, ResourceLoader } from './resources.js'
+import { Resources, ResourceLoader } from '../resources.js'
 
 export class Game extends Engine {
 
