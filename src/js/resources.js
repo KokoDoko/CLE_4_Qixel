d6aa13644b1708dcaf26df6d977a45fb053aba70
maudd <1091860@hr.nl>
2025-06-13 14:34:29 +0200

    ADD lab background

--- src/js/resources.js
+++ src/js/resources.js
@@ -6,6 +6,11 @@ const Resources = {
     Player: new ImageSource('images/player-sprite.png'),
     Bubble: new ImageSource('images/bubble.png'),
     Mine: new ImageSource('images/mine.png'),
+    LabBackground: new ImageSource('images/lab/labvloerbasic.png'),
+    LabTable2: new ImageSource('images/lab/labtable2.png'),
+    LabTableLong: new ImageSource('images/lab/labtablelong.png'),
+    LabTableMiddle: new ImageSource('images/lab/labtablemiddle.png'),
+
 }
 
 const ResourceLoader = new Loader()
