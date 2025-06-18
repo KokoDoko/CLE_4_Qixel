a5d45ef1298e199a7520339b562590ab1a64fb45
maudd <1091860@hr.nl>
2025-06-18 11:14:44 +0200

    merged

--- src/js/moeras/swamp.js
+++ src/js/moeras/swamp.js
@@ -1,18 +1,35 @@
 import { Actor, BoundingBox, Scene, Vector } from "excalibur";
 import { Resources } from "../resources.js";
-import { Player } from "../player.js";
+import { Player } from "../player.js"
+import { Capybara } from "./capybara.js"
+import { UI } from "../UI.js"
 import { SwampBorderLeft } from "./swampBorderLeft.js";
 import { SwampBorderRight } from "./swampBorderRight.js";
 import { SwampBorderTop } from "./swampBorderTop.js";
 import { SwampBorderBottom } from "./swampBorderBottom.js";
-import { SwampDoor } from "./door.js";
+import { BlueBush } from "./bluebush.js"
+import { SwampRose } from "./swampRose.js"
+import { SwampDoor } from "../lab/swampDoor.js";
 
 export class MoerasScene extends Scene {
     onActivate(ctx) {
         this.clear();
+        const bubble = new Actor({ pos: new Vector(640, 360) });
+        bubble.graphics.use(Resources.Bubble.toSprite());
+        bubble.on("pointerup", () => {
+            ctx.engine.goToScene('game');
+        });
+        this.add(bubble);
 
-        let swampDoor = new SwampDoor();
-        this.add(swampDoor)
+        this.obstaclePositions = [];
+
+        this.positionObstacle(BlueBush, 7, this.obstaclePositions)
+
+
+        let capybara = new Capybara()
+        this.add(capybara)
+
+        this.positionObstacle(SwampRose, 1, this.obstaclePositions)
 
         const player = new Player();
         this.pos = new Vector(300, 60);
@@ -20,6 +37,14 @@ export class MoerasScene extends Scene {
         this.height = new Vector(30, 0)
 
         this.add(player)
+
+        const playerUI = new UI(player)
+        this.add(playerUI)
+
+        this.clear();
+
+        let swampDoor = new SwampDoor();
+        this.add(swampDoor)
 
         const minX = 0;
         const maxX = 1240;
@@ -41,6 +66,43 @@ export class MoerasScene extends Scene {
 
         let swampBorderBottom = new SwampBorderBottom();
         this.add(swampBorderBottom)
+
+    }
+
+    positionObstacle(ObstacleClass, number, positions) {
+        const obstacleCount = number
+        const width = 1240
+        const height = 920
+        // let minDistance = 100;
+        // let positions = [];
+        const obstacleSize = 100
+        const margin = obstacleSize / 2;
+        const minDistance = obstacleSize
+
+        function isFarEnough(x, y) {
+            for (let i = 0; i < positions.length; i++) {
+                let pos = positions[i];
+                let dx = pos.x - x;
+                let dy = pos.y - y;
+                if (Math.sqrt(dx * dx + dy * dy) < minDistance) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        let placed = 0;
+        while (placed < obstacleCount) {
+            const x = margin + Math.random() * (width - 2 * margin)
+            const y = margin + Math.random() * (height - 2 * margin)
+            if (isFarEnough(x, y)) {
+                let obstacle = new ObstacleClass()
+                obstacle.pos = new Vector(x, y)
+                this.add(obstacle)
+                positions.push({ x: x, y: y });
+                placed++;
+            }
+        }
 
     }
 }
